2bcf467ac64490b612a4b2e182a9897bb0dda635
EmrickF <erik.finnberg07@gmail.com>
2024-11-15 12:40:30 +0200

    lol hej!

---
+++ front/script.js
@@ -0,0 +1,10 @@
+const cookie = document.getElementById('cookie')
+let clicks = 0
+const showScore = document.getElementById('clicks')
+
+
+cookie.addEventListener('click', ()=>{
+    clicks++
+    showScore.innerHTML= clicks
+  
+    })
