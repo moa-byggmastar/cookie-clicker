4a39588a511e3625cd7b7e9cdce686171d42b974
EmrickF <erik.finnberg07@gmail.com>
2024-11-18 12:00:10 +0200

    SIGMAAAAA

--- front/script.js
+++ front/script.js
@@ -1,10 +1,18 @@
 const cookie = document.getElementById('cookie')
 let clicks = 0
 const showScore = document.getElementById('clicks')
+const upgrade = document.getElementById('upgrade')
 
+cookie.addEventListener('click', () => {
+    clicks++;
+    showScore.innerHTML = clicks;
+    console.log("cock2")
+    if (clicks >= 50){
+        upgrade.style.backgroundColor = "#d92323"
+        console.log("cock1")
+    }
 
-cookie.addEventListener('click', ()=>{
-    clicks++
-    showScore.innerHTML= clicks
-  
-    })
+});
+function UpgradeFunction(){
+}
+
