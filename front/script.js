3a4cc80926157d3aa3b0210f9943b75d5da1649c
EmrickF <erik.finnberg07@gmail.com>
2024-11-18 12:50:33 +0200

    final push för idag :P

--- front/script.js
+++ front/script.js
@@ -7,19 +7,30 @@ const upgrade = document.getElementById('upgrade')
 cookie.addEventListener('click', () => {
     clicks +=markiplier ;
     showScore.innerHTML = clicks;
-    if (clicks >= 50){
+    if (clicks >= 50 ){
         upgrade.style.backgroundColor = "#d92323"
     }
-
+    if (clicks >= 200){
+        upgrade.style.backgroundColor = "#006400"
+    }
+    if (clicks >= 500){
+        upgrade.style.backgroundColor = "#FC0FC0"
+    }
 });
 function UpgradeFunction(){
     if (clicks >= 50 && clicks < 200){
      markiplier = 4
      cookie.querySelector("img").src = "wmonster.png"
     }
-    else if(clicks >= 200){
+     if(clicks >= 200 && clicks < 500){
         markiplier = 8
         cookie.querySelector("img").src = "badapplemonster.png"
        }
+       if(clicks >= 500 && clicks < 2000){
+        markiplier = 12
+        cookie.querySelector("img").src = "pipelinepunchmonster.png"
+       }
+    
 }
+ 
 
