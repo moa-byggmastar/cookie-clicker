6bb48ee3066a58a4b482dcd18ecaded8564bd1c9
EmrickF <erik.finnberg07@gmail.com>
2024-11-18 10:13:16 +0200

    uhhh JA VEJTTT INT 🤣🤣🤣🤣

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,13 @@
 const express = require("express")
-
+server.use(express.static("front"))
 const server = express();
 
 const port = 3000
 
+server.get('/', (req, res)=> {
+  
+  });
+  
 
 
 server.listen(port, (req, res)=>{
