6ecfd11760ee2b9aeb07b52c41d33046beb8c29c
EmrickF <erik.finnberg07@gmail.com>
2024-11-18 12:26:17 +0200

    pushing positivity

--- front/script.js
+++ front/script.js
@@ -1,18 +1,25 @@
 const cookie = document.getElementById('cookie')
 let clicks = 0
+let markiplier = 1
 const showScore = document.getElementById('clicks')
 const upgrade = document.getElementById('upgrade')
 
 cookie.addEventListener('click', () => {
-    clicks++;
+    clicks +=markiplier ;
     showScore.innerHTML = clicks;
-    console.log("cock2")
     if (clicks >= 50){
         upgrade.style.backgroundColor = "#d92323"
-        console.log("cock1")
     }
 
 });
 function UpgradeFunction(){
+    if (clicks >= 50 && clicks < 200){
+     markiplier = 4
+     cookie.querySelector("img").src = "wmonster.png"
+    }
+    else if(clicks >= 200){
+        markiplier = 8
+        cookie.querySelector("img").src = "badapplemonster.png"
+       }
 }
 
