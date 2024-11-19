f8b2bc5921ba46dea588f3a046ff650a6dd60eb0
EmrickF <erik.finnberg07@gmail.com>
2024-11-19 09:44:10 +0200

    Kidney stone event + buttons funkar bättre

--- front/script.js
+++ front/script.js
@@ -8,13 +8,20 @@ cookie.addEventListener('click', () => {
     clicks +=markiplier ;
     showScore.innerHTML = clicks;
     if (clicks >= 50 ){
-        upgrade.style.backgroundColor = "#d92323"
+        upgrade.style.backgroundColor = "#FFFFFF"
+        document.getElementById("upgrade").innerHTML = "UPGRADE TO WHITE";
     }
     if (clicks >= 200){
         upgrade.style.backgroundColor = "#006400"
+        document.getElementById("upgrade").innerHTML = "UPGRADE TO BAD APPLE";
     }
     if (clicks >= 500){
         upgrade.style.backgroundColor = "#FC0FC0"
+        document.getElementById("upgrade").innerHTML = "UPGRADE TO PIPELINE PUNCH";
+    }
+    if (clicks >= 2000){
+        upgrade.style.backgroundColor = "#F9D199"
+        document.getElementById("upgrade").innerHTML = "UPGRADE TO PACIFIC PUNCH";
     }
 });
 function UpgradeFunction(){
@@ -30,7 +37,24 @@ function UpgradeFunction(){
         markiplier = 12
         cookie.querySelector("img").src = "pipelinepunchmonster.png"
        }
-    
+       if(clicks >= 2000 && clicks < 5000){
+        markiplier = 20
+        cookie.querySelector("img").src = "pacificpunchmonster.png"
+       }
+       
 }
- 
+function kidneyStone() {
+    let min = 5;
+    let max = 2000;
+    let rand = Math.floor(Math.random() * (max - min + 1) + min); // Generate random number between 5 - 2000
+    if (clicks >= 10000) {
+        clicks -= 10000; // Deduct clicks immediately
+        alert(`KIDNEY STONE ATTACK HAHAHA! (You lost 10000 clicks) Remaining clicks: ${clicks}`);
+    } else {
+        alert("KIDNEY STONE ATTACK HAHAHA! (But you are broke so I won't tax you)");
+    }
 
+    setTimeout(kidneyStone, rand * 2000);
+}
+
+kidneyStone();
