95f944c315beb7e3c3aeed69c46342b8395d3999
FakeColdMilk <aaron.nylund2@gmail.com>
2024-11-22 11:01:27 +0200

    fucked

--- front/script.js
+++ front/script.js
@@ -14,17 +14,18 @@ cookie.addEventListener('click', () => {
     console.log("cock2");
 
     if (clicks >= 50) {
-        upgrade.style.backgroundColor = "#ce4f4f";
-        console.log("cock1");
+        upgrade.style.backgroundColor = "#ce4f4f"
+        console.log("cock1")
     }
-
     if (clicks >= 200) {
-        upgrade.innerHTML = "UPGRADE TO BAD APPLE";
+        upgrade.innerHTML = "UPGRADE TO BAD APPLE"
     }
     if (clicks >= 5000) {
-        upgrade.style.backgroundColor = "#FFFF00";
-        upgrade.style.Color = "#000000";
+        upgrade.style.backgroundColor = "#FFFF00"
+        upgrade.style.Color = "#000000"
         document.getElementById("upgrade").innerHTML = "UPGRADE TO  THE DOCTOR";
+        upgrade.style.backgroundColor = "#FFFF00"
+        upgrade.style.Color = "#001000"
     }
 });
 
@@ -34,27 +35,27 @@ function UpgradeFunction() {
         cookie.querySelector("img").src = "wmonster.png";
     }
     else if (clicks >= 200) {
-        markiplier = 8;
-        cookie.querySelector("img").src = "badapplemonster.png";
-    }
-    if (clicks >= 500 && clicks < 2000) {
-        markiplier = 12;
-        cookie.querySelector("img").src = "pipelinepunchmonster.png";
-    }
-    if (clicks >= 2000 && clicks < 5000) {
-        markiplier = 20;
-        cookie.querySelector("img").src = "pacificpunchmonster.png";
-    }
-    if (clicks >= 5000 && clicks < 10000) {
-        markiplier = 40;
-        cookie.querySelector("img").src = "vr46monster.png";
-    }
+        markiplier = 8
+        cookie.querySelector("img").src = "badapplemonster.png"
+       }
+       if(clicks >= 500 && clicks < 2000){
+        markiplier = 12
+        cookie.querySelector("img").src = "pipelinepunchmonster.png"
+       }
+       if(clicks >= 2000 && clicks < 5000){
+        markiplier = 20
+        cookie.querySelector("img").src = "pacificpunchmonster.png"
+       }
+       if(clicks >= 5000 && clicks < 10000){
+        markiplier = 40
+        cookie.querySelector("img").src = "vr46monster.png"
+       }
+       
 }
-
 function kidneyStone() {
     let min = 5;
     let max = 2000;
-    let rand = Math.floor(Math.random() * (max - min + 1) + min); 
+    let rand = Math.floor(Math.random() * (max - min + 1) + min); // Generate random number between 5 - 2000
     if (clicks > 0) { 
         if (clicks >= 10000) {
             clicks -= 10000; 
@@ -63,16 +64,13 @@ function kidneyStone() {
             alert("KIDNEY STONE ATTACK HAHAHA! (But you are broke so I won't tax you)");
         }
 
-        showScore.innerHTML = clicks;  
+        showScore.innerHTML = clicks; 
     }
     setTimeout(rand);
 }
 
 kidneyStone();
-
-const minionSelect = document.getElementById('minion-select');
-const buyMinionButton = document.getElementById('buy-minion');
-const activeMinionsDiv = document.getElementById('active-minions');
+    
 
 const minions = {
     squirrel: { name: "Hyperactive Squirrel", cost: 100, cps: 2 },
